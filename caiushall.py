646816b10d6aa59edb75050d940497c12564ce6b
Joe Bateson <jdb75@cam.ac.uk>
2012-12-20 01:19:19 +0000

    Add is_authed and logout methods, refactor auth code

--- caiushall.py
+++ caiushall.py
@@ -3,6 +3,7 @@
 import requests
 import bs4
 
+# TODO: split into multiple classes for net and local methods
 class CaiusHall():
     """
     Main class for the Caius Hall desktop application.
@@ -14,6 +15,7 @@ class CaiusHall():
         CERTS: the string for the local path to a file containing the necessary SSL certs
         cookies: the list of cookies used when authenticating with Raven
         current_user: the string crsid of the last user to authenticate
+        session: the current requests.Session object to make requests with
     """
 
     def __init__(self):
@@ -25,77 +27,102 @@ class CaiusHall():
         self.cookies = None
         self.current_user = None
 
-    def auth(self, crsid, password, attempt=1):
+        # Construct the requests session object
+        self.session = requests.Session()
+        self.session.verify = self.CERTS
+
+    def auth(self, crsid, password):
         """
         Authenticates a user with the Caius Hall Booking system.
-
-        Includes checks to ascertain whether the user is already logged in.
 
         Args:
             crsid: The user's crsid (Raven username)
             password: The user's password
-            attempt: The current count of attempts in this loop (prevents perpetual looping)
 
         Returns:
             A boolean value corresponding to the success or failure of the authentication attempt.
             Note: If the user is already logged in, True is returned.
 
         """
-        if (attempt > 2):
-            print('Error: Something sent the authentication process into a loop!')
-            return False
-        elif (self.cookies):
-            # we have cookies set for self.current_user
-            if (crsid == self.current_user):
-                # requested user already has cookies
-                # let's check if our session is still alive
-                # TODO: possible to check session without using a request?
-                r = requests.get(self.HALL_URL, cookies=self.cookies, verify=self.CERTS)
-                if(r.status_code == requests.codes.ok):
-                    if (r.url != self.HALL_URL):
-                        # our session has expired, try again
-                        self.cookies = None
-                        self.current_user = None
-                        print('Session appears to have expired for current user, trying again...')
-                        self.auth(crsid, password, attempt+1)
-                    else:
-                        print('User ' + crsid + ' is already logged in.')
-                        return True
-                else:
-                    print('Error: Authentication failed - ' + r.headers.status)
-                    return False
+
+        if (crsid == self.current_user):
+            # our current_user wants to re-login
+            if (self.is_authed()):
+                # user is already logged in, no action needed
+                print('User ' + crsid + ' is already logged in.')
+                return True
+                # no cookies set, need to log in with Raven
+
+        # Begin login procedure here, both for different users and expired sessions
+
+        data = {'userid': crsid, 'pwd': password}
+        r = self.session.post(self.RAVEN_AUTH_PAGE, data=data, allow_redirects=False)
+
+        if(r.status_code == 302):
+            # server sent us a 302 redirect, check that the redirect is taking us to
+            # the status page
+            if (r.headers['location'] == self.RAVEN_STATUS_PAGE):
+                # if all goes well, we should end up here
+                # set the cookies for future auths and the current user's crsid
+                self.cookies = r.cookies
+                self.current_user = crsid
+                print("Successfully logged in user " + crsid + '.')
+                return True
             else:
-                # log in with different crsid, remove cookies
+                # find the error using html parsing (only parse span.error)
+                # TODO: any way to streamline this code?
+                error_span_tags = bs4.SoupStrainer('span', class_='error')
+                soup = bs4.BeautifulSoup(r.text, parse_only=error_span_tags)
+                errors = soup.find_all('span', class_='error')
+                if (len(errors) > 0):
+                    print ('Error: Server responded - ' + errors[0].text)
+                else:
+                    print('Error: Received a good response from server, but the page returned was unexpected.')
+                return False
+        else:
+            print('Error: Authentication failed - server responded with error code ' + str(r.status_code))
+            return False
+
+    def is_authed(self):
+        """
+        Checks if there is a user currently authenticated with the server.
+
+        Cookies are checked, and if they exist, we make a GET request to the hall
+        server and determine whether we are logged in from the response URL.
+
+        Returns:
+            A boolean value. True if there is a user currently authenticated, and false otherwise.
+        """
+
+        if (not self.cookies):
+            # no cookies, not logged in
+            return False
+        else:
+            # make a request to the hall server and check its url
+            r = self.session.get(self.HALL_URL, cookies=self.cookies)
+            if (self.HALL_URL == r.url):
+                return True
+            else:
                 self.cookies = None
                 self.current_user = None
-                print('Different user requested, logging into new account...')
-                self.auth(crsid, password, attempt+1)
-
-        else:
-            # no cookies set, need to log in with Raven
-            data = {'userid': crsid, 'pwd': password}
-            r = requests.post(self.RAVEN_AUTH_PAGE, params=data, verify=self.CERTS)
-
-            if(r.status_code == requests.codes.ok):
-                # server sent us a 200 OK
-                if (r.url == self.RAVEN_STATUS_PAGE):
-                    # if all goes well, we should end up here
-                    # set the cookies for future auths and the current user's crsid
-                    self.cookies = r.cookies
-                    self.current_user = crsid
-                    print("Successfully logged in user " + crsid + '.')
-                    return True
-                else:
-                    # find the error using html parsing (only parse span#error)
-                    # TODO: any way to streamline this code?
-                    error_span_tags = bs4.SoupStrainer('span', class_='error')
-                    soup = bs4.BeautifulSoup(r.text, parse_only=error_span_tags)
-                    errors = soup.find_all('span', class_='error')
-                    if (len(errors) > 0):
-                        print ('Error: Server responded - ' + errors[0].text)
-                    else:
-                        print('Error: Received a good response from server, but the page returned was unexpected.')
-                    return False
-            else:
-                print('Error: Authentication failed - ' + r.headers.status)
                 return False
+
+
+    def logout(self):
+        """
+        Logs the current user out of the server.
+
+        We create a new session object and remove the previous cookies. The value of current_user
+        is also reset.
+        """
+
+        # Create a new session object
+        self.session = requests.Session()
+        self.session.verify = self.CERTS
+
+        self.current_user = None
+        self.cookies = None
+        print('Logged out of Raven.')
+
+# TODO: unit tests
+
